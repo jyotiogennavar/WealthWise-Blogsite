b240f62c62cb7f1355eb20a1dab83a581fb5ebfa
Jyoti <jyoti.ogennavar@xecurify.com>
2024-11-05 15:29:39 +0530

    Enhance BlogCard and BlogCardGrid components with improved styles and animations; update image handling and layout for better responsiveness

--- app/components/BlogCard/BlogCard.tsx
+++ app/components/BlogCard/BlogCard.tsx
@@ -1,9 +1,10 @@
+"use client";
 import React from "react";
 import clsx from "clsx";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 import styles from "./BlogCard.module.css";
-
 interface BlogCardProps {
   imageUrl: string;
   tag: string;
@@ -12,16 +13,23 @@ interface BlogCardProps {
 }
 
 function BlogCard({
-  imageUrl = "/public/image/blog_card.png",
+  imageUrl = "/image/blog_card.png",
   tag = "Investing",
   title = "Financial Goals: How to Set and Achieve Them in Any Stage of Life",
   description = "From short-term savings to long-term investments, find out how to set realistic financial goals and build a plan to achieve them.",
 }: BlogCardProps) {
   return (
     <div className={clsx(styles.wrapper)}>
-      <div className={clsx(styles.blogCardImgContainer)}>
-        <Image src={imageUrl} alt={title} layout="fill" objectFit="cover" />
-      </div>
+      <motion.div
+        className={clsx(styles.blogCardImgContainer)}
+        whileHover={{ scale: 1.03 }} // Slight zoom effect
+        transition={{ duration: 0.3 }} // Smooth transition
+      >
+        <Image src={imageUrl} alt={title} fill className="object-cover" />
+      </motion.div>
+      {/* <div className={clsx(styles.blogCardImgContainer)}>
+        <Image src={imageUrl} alt={title} fill className="object-cover" />
+      </div> */}
       <div className={clsx(styles.blogCardTextContainer)}>
         <span className={clsx(styles.blogTag)}>{tag}</span>
         <h3 className={clsx(styles.blogCardTitle)}>{title}</h3>
--- app/components/BlogCardGrid/BlogCardGrid.tsx
+++ app/components/BlogCardGrid/BlogCardGrid.tsx
@@ -10,19 +10,38 @@ function BlogCardGrid() {
       <h2>Latest Posts</h2>
       <div className={clsx(styles.grid)}>
         <BlogCard
-          imageUrl="/public/image/blog_card.png"
+          imageUrl="/image/image_1.png"
           tag="Personal Finance"
           title="Building a Strong Financial Foundation: Essential Steps for Beginners"
           description="Learn how to take control of your money with simple strategies to save, invest, and manage debt effectively."
         />
         <BlogCard
-          imageUrl="/public/image/blog_card.png"
+          imageUrl="/image/image_2.png"
           tag="Budgeting"
           title="Mastering the Art of Budgeting: How to Track and Cut Expenses"
           description="Discover practical tips to create a budget that works for you, and make smarter financial decisions without feeling restricted."
         />
         <BlogCard
-          imageUrl="/public/image/blog_card.png"
+          imageUrl="/image/image_3.png"
+          tag="Investing"
+          title="Investing 101: A Beginner’s Guide to Growing Your Wealth"
+          description="Unpack the basics of investing and explore low-risk strategies to start building your financial portfolio today."
+        />
+
+        <BlogCard
+          imageUrl="/image/image_1.png"
+          tag="Personal Finance"
+          title="Building a Strong Financial Foundation: Essential Steps for Beginners"
+          description="Learn how to take control of your money with simple strategies to save, invest, and manage debt effectively."
+        />
+        <BlogCard
+          imageUrl="/image/image_2.png"
+          tag="Budgeting"
+          title="Mastering the Art of Budgeting: How to Track and Cut Expenses"
+          description="Discover practical tips to create a budget that works for you, and make smarter financial decisions without feeling restricted."
+        />
+        <BlogCard
+          imageUrl="/image/image_3.png"
           tag="Investing"
           title="Investing 101: A Beginner’s Guide to Growing Your Wealth"
           description="Unpack the basics of investing and explore low-risk strategies to start building your financial portfolio today."
